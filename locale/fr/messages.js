e440f6acab0c0b472f9f31f0a5f7b064865bb569
Mike Williamson <mike.williamson@tbs-sct.gc.ca>
2017-09-18 18:55:35 -0400

    Update translations
    yarn extract && yarn compile

--- locale/fr/messages.js
+++ locale/fr/messages.js
@@ -1,1 +1,1 @@
-module.exports={l:{"p":function(n,ord){if(ord)return n==1?"one":"other";return n>=0&&n<2?"one":"other"}},m:{"Pay your fees":"payez vos frais"}};
+module.exports={l:{"p":function(n,ord){if(ord)return n==1?"one":"other";return n>=0&&n<2?"one":"other"}},m:{"Pay your fees":"Payez vos frais"}};
