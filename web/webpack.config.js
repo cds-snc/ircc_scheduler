8dfd83472ecd29e4521d1ec1f36733f0df074cd3
Mike Williamson <mike.williamson@tbs-sct.gc.ca>
2017-10-13 13:40:14 -0400

    Add source maps in developement mode.

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -3,15 +3,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = env => {
   return {
+    devtool: env === 'development' ? 'source-map' : 'none',
     entry: './src/index.js',
     output: {
       filename: 'bundle.js',
       path: path.join(__dirname, 'dist'),
     },
-    plugins: [new HtmlWebpackPlugin({
-			template: require('html-webpack-template'),
-			appMountId: 'app',
-		})],
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: require('html-webpack-template'),
+        appMountId: 'app',
+      }),
+    ],
     module: {
       loaders: [
         { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
