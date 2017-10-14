7e5bb1da42a4cdb84b34986db73c001ee8e7082f
Mike Williamson <mike.williamson@tbs-sct.gc.ca>
2017-10-13 22:16:27 -0400

    Add missing translations

--- web/src/components/MultiStep.js
+++ web/src/components/MultiStep.js
@@ -7,34 +7,52 @@ import PaymentButton from './PaymentButton'
 
 const MultiStep = () => (
   <div>
-    <h3>Pay your fees</h3>
+    <h3>
+      <Trans>Pay your fees</Trans>
+    </h3>
     <Panel width="100%" title={<Trans>How to pay your fees</Trans>}>
       <div style={{ padding: '1em 1em' }}>
         <Wizard>
           <Steps>
             <Step path="merlin">
-              <h1>Question 1</h1>
+              <h1>
+                <Trans>Question 1</Trans>
+              </h1>
               <Navigation
-                render={({ next }) => <Button onClick={next}>Next</Button>}
+                render={({ next }) => (
+                  <Button onClick={next}>
+                    <Trans>Next</Trans>
+                  </Button>
+                )}
               />
             </Step>
             <Step path="gandalf">
-              <h1>Question 2</h1>
+              <h1>
+                <Trans>Question 2</Trans>
+              </h1>
               <Navigation
                 render={({ next, previous }) => (
                   <div>
-                    <Button onClick={next}>Next</Button>
-                    <Button onClick={previous}>Previous</Button>
+                    <Button onClick={next}>
+                      <Trans>Next</Trans>
+                    </Button>
+                    <Button onClick={previous}>
+                      <Trans>Previous</Trans>
+                    </Button>
                   </div>
                 )}
               />
             </Step>
             <Step path="dumbledore">
-              <h1>Question 3</h1>
+              <h1>
+                <Trans>Question 3</Trans>
+              </h1>
               <PaymentButton />
               <Navigation
                 render={({ previous }) => (
-                  <Button onClick={previous}>Previous</Button>
+                  <Button onClick={previous}>
+                    <Trans>Previous</Trans>
+                  </Button>
                 )}
               />
             </Step>
